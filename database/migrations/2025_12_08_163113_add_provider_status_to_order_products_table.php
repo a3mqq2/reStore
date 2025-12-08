a004277b6d7e29cb518c2692452ed3c93f64cb0c
a3mqq2 <aishaaltery03@gmail.com>
2025-12-08 16:32:34 +0200

    updats

---
+++ database/migrations/2025_12_08_163113_add_provider_status_to_order_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->string('provider_status')->nullable()->after('cost');
+            $table->text('provider_response')->nullable()->after('provider_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->dropColumn(['provider_status', 'provider_response']);
+        });
+    }
+};
